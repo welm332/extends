cf245c5739f4cf94de0e3c3a4f2f73a00cc09efb
welm <haise0125@gmail.com>
2021-10-25 20:25:51 +0900

    create icon 	opend dir 	create file or dir button 	desin

--- dir/dir.js
+++ dir/dir.js
@@ -1,48 +1,143 @@
 // const beforefunc = open_args;
 window.api.create_local_shk("Ctrl+B");
 window.api.on("Ctrl+B", create_side);
+
+window.api.create_local_shk("Ctrl+C");
+window.api.on("Ctrl+C", copy_and_peast_listen);
+focusFlag = true;
+window.addEventListener("click" ,()=>{focusFlag = false});
+function copy_and_peast_listen(){
+    if(select_element !== null){
+        if(! focusFlag){
+            return false;
+        }
+        window.api.create_local_shk("Ctrl+V");
+        window.api.on("Ctrl+V", ()=>{
+            let i = 1;
+            const dirs = fs.readdirSync(window.requires.path.dirname(select_element.dataset.fullpath));
+            const file_basename = select_element.textContent;
+            while(true){
+                dot_rindex = file_basename.lastIndexOf(".");
+                if(dot_rindex === -1){
+                    dot_rindex = file_basename.length 
+                }
+                console.log(file_basename);
+                cp_name = file_basename.substring(0, dot_rindex)+" copy"+(i !== 1 ? ` ${i}`:"")+window.requires.path.extname(file_basename);
+                console.log(cp_name);
+                if(dirs.indexOf(cp_name) === -1){
+                    console.log(window.requires.fs.copyFileSync(select_element.dataset.fullpath, window.requires.path.dirname(select_element.dataset.fullpath)+"/"+cp_name));
+                    break
+                }
+                i++;
+                
+            }
+        });
+
+    }
+}
+let select_element = null
 async function create_element(path, type, div, nest=0, intertPoint=null){
-    bt = document.createElement("button");
+    const element = document.createElement("div");
+    element.className = type;
+    console.log(type);
+    console.log(type === "dir");
+    console.log(type == "dir");
+    element.textContent = window.requires.path.basename(path);
+    if(type === "dir"){
+        const file_inputer = document.createElement("input");
+        file_inputer.type = "image";
+        file_inputer.src = window.requires.dirname+"/..//extends/dir/file_icon.png";
+        // const width = (element.style.height * input.style.width) / input.style.height;
+        file_inputer.style.height = "20px";//element.style.height;
+        file_inputer.style.width = "20px";//width;
+        file_inputer.className = "file_name_inputer";
+        file_inputer.onclick = (event1)=>{
+            // return 
+            fname_input = document.createElement("input");
+            fname_input.onkeydown = (event2)=>{
+                // console.log(event2);
+                if(event2.key === "Enter"){
+                    window.requires.fs.writeFileSync(path+"/"+event2.target.value, "");
+                    event2.target.remove();
+                }
+            };
+            div.insertBefore(fname_input, element.nextSibling);
+            event1.stopPropagation();
+        };
+        
+        element.appendChild(file_inputer);
+        
+        
+        
+        const dir_inputer = document.createElement("input");
+        dir_inputer.type = "image";
+        dir_inputer.src = window.requires.dirname+"/..//extends/dir/dir_icon.png";
+        // const width = (element.style.height * input.style.width) / input.style.height;
+        dir_inputer.style.height = "20px";//element.style.height;
+        dir_inputer.style.width = "20px";//width;
+        dir_inputer.className = "dir_name_inputer";
+        dir_inputer.onclick = (event1)=>{
+            // return 
+            fname_input = document.createElement("input");
+            fname_input.onkeydown = (event2)=>{
+                // console.log(event2);
+                if(event2.key === "Enter"){
+                    window.requires.fs.mkdirSync(path+"/"+event2.target.value);
+                    event2.target.remove();
+                }
+            };
+            div.insertBefore(fname_input, element.nextSibling);
+            event1.stopPropagation();
+        };
+        
+        
+        element.appendChild(dir_inputer);
+    }
     path = window.requires.path.resolve(path);
-    bt.textContent = window.requires.path.basename(path);
-    bt.style.marginLeft = `${nest*15}px`;
-    // bt.style.width = "300px"; 
-    // bt.style.height = "40px";
-    bt.dataset.status = "closed";
-    bt.dataset.fullpath = window.requires.path.resolve(path);
-    bt.style.display = "block";
-    bt.onclick = async (event)=>{
-        const button = event.target;
+    element.style.marginLeft = `${nest*15}px`;
+    // element.style.width = "300px"; 
+    // element.style.height = "40px";
+    element.dataset.status = "closed";
+    element.dataset.fullpath = window.requires.path.resolve(path);
+    element.style.display = "block";
+    element.onclick = async (event)=>{
+        focusFlag = true;
+        const target = event.target;
         if(type == "file"){
+            if(select_element !== null){
+                select_element.style.color = "black";
+            }
+            target.style.color = "blue";
+            select_element = target;
             console.log(path);
             let em = create_tab().dataset.fullpath;
             const fullpath = get_path(path);
             readFile(fullpath, em);
-        }else if(button.dataset.status === "closed"){
-            button.style.background  = "gray";
-            button.dataset.status = "opend";
+        }else if(target.dataset.status === "closed"){
+            target.style.background  = "gray";
+            target.dataset.status = "opend";
             let children = fs.readdirSync(path);
             children =children.map((child)=>path+"/"+child);
             console.log(children);
             const DirFlags = await window.api.isDirs(children);
             for(let i=0,len=children.length;i<len;i++){
-               create_element(children[i], DirFlags[i] ? "dir":"file", div, nest+1, button);
+               create_element(children[i], DirFlags[i] ? "dir":"file", div, nest+1, target);
             }
         }else{
-            button.style.background  = "white";
-            const delist = Array(...div.children).filter((em)=> window.requires.path.dirname(em.dataset.fullpath) ===  button.dataset.fullpath)
+            target.style.background  = "white";
+            const delist = Array(...div.children).filter((em)=> window.requires.path.dirname(em.dataset.fullpath) ===  target.dataset.fullpath)
             console.log(delist);
             for(const em of delist){
                 em.remove();
             }
-            button.dataset.status = "closed";
+            target.dataset.status = "closed";
         }
         
     }
     if(intertPoint === null){
-        div.appendChild(bt);
+        div.appendChild(element);
     }else{
-        div.insertBefore(bt, intertPoint.nextSibling)
+        div.insertBefore(element, intertPoint.nextSibling)
     }
     // div.style.overflow = "scroll scroll";
 }
@@ -69,14 +164,15 @@ async function open_dir(){
 window.addEventListener("load", open_dir);
     
 function create_side(){
+    // return;
     let div = document.createElement('div');
     a = document.querySelector("#dialogspace");
     div.id = "dir_side";
     div.style.position = "absolute";
     div.style.top = "100px";
     document.body.insertBefore( div,a);
-    if(document.querySelector("#input_area").style.padding !== "34px 0px 20px 500px"){
-        document.querySelector("#input_area").style.padding = "34px 0px 20px 500px";
+    if(document.querySelector("#input_area").style.padding !== "34px 0px 20px 200px"){
+        document.querySelector("#input_area").style.padding = "34px 0px 20px 200px";
     }else{
         document.querySelector("#input_area").style.padding = "34px 0px 20px 0px";
     }
